f8a105b6650d3fb7a7146b1863ff3f909f691892
EnoshTs <enosh.tsur@haaretz.co.il>
2020-05-19 17:54:43 +0300

    coupondemo

---
+++ src/main/java/com/enosh/springdeom/SpringDeomApplication.java
@@ -0,0 +1,13 @@
+package com.enosh.springdeom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeomApplication.class, args);
+    }
+
+}
