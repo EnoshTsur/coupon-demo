c7678b4740913ed566a36b3305e3fbb195d67c85
EnoshTs <enosh.tsur@haaretz.co.il>
2020-05-19 18:45:41 +0300

    hi

--- src/main/java/com/enosh/springdeom/model/BaseEntity.java
+++ src/main/java/com/enosh/springdeom/model/BaseEntity.java
@@ -1,12 +1,9 @@
 package com.enosh.springdeom.model;
 
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 
-@Getter
-@Setter
+
 @MappedSuperclass
 public class BaseEntity {
 
@@ -14,4 +11,6 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, unique = true)
     protected Long id;
+
 }
+
